30be798893a9e617e0f5880c622f8b1975c4c094
Valentin Kolesnikov <dima.nedelin2704@gmail.com>
2025-07-26 21:14:18 +0300

    I think that's a final update

--- List.py
+++ List.py
@@ -1,5 +1,5 @@
 parameters = int(
-    input("Do you need " "1" "- the main info, " "2" " - the main info and additional info?: ")
+    input("\nDo you need " "1" "- the main info, " "2" " - the main info and additional info?: ")
 )
 
 names = []
@@ -8,7 +8,7 @@ a = 1
 stopped = False
 
 while True:
-    name = input("Info or stop: ")
+    name = input("\nInfo or stop: ")
     names.append(name)
 
     if name.lower() in ["stop", "enough", "yes", "-"]:
@@ -17,17 +17,18 @@ while True:
         break
 
     if parameters == 2 and not stopped:
-        point = input("Additional info: ")
+        point = input("\n-Additional info: ")
         additionalp.append(point)
 
-print("")
-print("List:")
+print("\n------------------------List:------------------------\n")
 if parameters == 1:
     for first in names:
-        print(f"{a}) {first}")
+        print(f"    {a}) {first}\n")
         a += 1
 
 elif parameters == 2:
     for first, second in zip(names, additionalp):
-        print(f" {a}) {first} - {second}")
+        print(f"    {a}) {first} -- {second}\n")
         a += 1
+
+print("\n-----------------------------------------------------\n")
